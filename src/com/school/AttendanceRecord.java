07a1ea1b1b65f4967811b8072e8a0d9b4f670c36
JoRobert137 <rjohn.robert@kalvium.community>
2025-07-04 17:46:56 +0530

    feat: Implement encapsulation and basic attendance recording (Part 4)

--- src/com/school/AttendanceRecord.java
+++ src/com/school/AttendanceRecord.java
@@ -1,3 +1,5 @@
+package com.school;
+
 public class AttendanceRecord {
     private
     int studentId;
--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -3,15 +3,15 @@ package com.school;
 
 public class Course {
     private static int nextCourseIdCounter = 101;
-    String courseId; // e.g., "CS101"
+    int courseId; // e.g., "CS101"
     String courseName;
 
     public Course(String courseName) {
-        this.courseId = "CS" + nextCourseIdCounter++;
+        this.courseId = nextCourseIdCounter++;
         this.courseName = courseName;
     }
 
-    public String getCourseId() {
+    public int getCourseId() {
         return courseId;
     }
 
